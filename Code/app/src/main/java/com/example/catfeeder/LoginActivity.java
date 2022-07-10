5eac6e97012878bc6f8d73cada96d80d4eddc8f0
Cesarq19 <cepaquin@espol.edu.ec>
2022-07-09 20:21:35 -0500

    Principio de estructura logica

--- Code/app/src/main/java/com/example/catfeeder/LoginActivity.java
+++ Code/app/src/main/java/com/example/catfeeder/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.catfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void login(View view){
+
     }
 }
